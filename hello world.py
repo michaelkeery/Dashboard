b30c0f1ed615acc134f4ad344f4be0a1cf5af3d3
unknown <michael.keery@gmail.com>
2025-03-12 14:35:59 -0400

    Test/start/learning

---
+++ hello world.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.geometry('550x200')
+window.title("This is a cruel world being run by Brian")
+
+# Create a text box
+#text_box = tk.Text(window, height=5, width=40)
+#text_box.pack()
+
+window.mainloop()
