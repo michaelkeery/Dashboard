ada91bd234635e3665becdca4fc06fb9cc38149c
michael keery <michael.keery@gmail.com>
2025-03-12 14:43:30 -0400

    new

--- hello world.py
+++ hello world.py
@@ -4,6 +4,8 @@ window = tk.Tk()
 window.geometry('550x200')
 window.title("This is a cruel world being run by Brian")
 
+
+
 # Create a text box
 #text_box = tk.Text(window, height=5, width=40)
 #text_box.pack()
